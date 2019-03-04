88d26b7770e3e943e8d82ef8eb1cda94669eae10
Tom Davies <tom@tomdavies.net>
2019-03-04 13:24:06 +0000

    remove placeholder vue code and run lint and test on pre-push hook

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,7 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+// import { shallowMount } from "@vue/test-utils";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("main.js", () => {
+  it("has a dummy test", () => {
+    expect(true).toEqual(true);
   });
 });
