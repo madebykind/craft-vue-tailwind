90f13078c2235c920bddc7f4302626f440f8b18f
Tom Davies <tom@tomdavies.net>
2019-03-08 15:07:02 +0000

    more script refactoring

--- scripts/lib/toDotEnvString.js
+++ scripts/lib/toDotEnvString.js
@@ -6,11 +6,14 @@ const toConstantName = (str) =>
       `_${$1.toLowerCase()}`)).toUpperCase();
 
 
-function toDotEnvString (environmentVariables) {
+const conditionallyWrapQuotes = (str) =>
+  /\s+/.test(str) ? `"${str}"` : str;
+
+const toDotEnvString = (environmentVariables) => {
   let dotEnvDocument = "";
 
   Object.keys(environmentVariables).forEach(envVar => {
-    dotEnvDocument += util.format("%s=%s\r\n", toConstantName(envVar), environmentVariables[envVar])
+    dotEnvDocument += util.format("%s=%s\r\n", toConstantName(envVar), conditionallyWrapQuotes(environmentVariables[envVar]))
   });
 
   return dotEnvDocument;
