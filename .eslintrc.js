ebe0f912d5c5511261981ad87b6fb8420dbaaf22
Tom Davies <tom@tomdavies.net>
2020-01-24 10:01:15 +0000

    make prettier errors an eslint warn, not an error

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   extends: ["@vue/airbnb", "plugin:vue/recommended", "plugin:prettier/recommended", "prettier/vue"],
   plugins: ["vue", "prettier"],
   rules: {
-    "prettier/prettier": 2,
+    "prettier/prettier": 1,
     "no-console": isProduction ? 2 : 0,
     "no-debugger": isProduction ? 2 : 0,
     "import/extensions": 0,
